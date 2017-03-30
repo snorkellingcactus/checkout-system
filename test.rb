6f0fc52d7c31eb3d4fac846b777ebd27509cfcc4
Nadal Gonzalo García Zavala <snorkellingcactus@gmail.com>
2017-03-30 03:23:18 -0300

    Really the final version.

--- test.rb
+++ test.rb
@@ -3,6 +3,7 @@
 require 'minitest/autorun'
 
 require_relative 'Checkout'
+
 require_relative 'Rule/RuleLavender'
 require_relative 'Rule/RuleSixtyOrMore'
 
@@ -10,47 +11,44 @@ require_relative 'Product/ProductLavenderHeart'
 require_relative 'Product/ProductPersCuff'
 require_relative 'Product/ProductKidsT'
 
-describe Checkout do
-	describe "test_examples" do
-		items={
-			"001"=> ProductLavenderHeart.new(),
-			"002"=> ProductPersCuff.new(),
-			"003"=> ProductKidsT.new()
-		}
+describe 'Checkout_test_examples' do
+	items={
+		'001'=> ProductLavenderHeart.new(),
+		'002'=> ProductPersCuff.new(),
+		'003'=> ProductKidsT.new()
+	}
 
-		examples=[
-			[ "001", "002", "003" ],
-			[ "001", "003", "001" ],
-			[ "001", "002", "001", "003" ]
+	examples=[
+		[ '001', '002', '003' ],
+		[ '001', '003', '001' ],
+		[ '001', '002', '001', '003' ],
+		[ '001', '002', '001', '003', '001' ]
+	]
+
+	results=[ 66.78, 36.95, 73.76, 81.40 ]
+
+	co=Checkout.new(
+			[
+				RuleLavender.new(),
+				RuleSixtyOrMore.new()
 		]
+	)
 
-		results=[ 66.78, 36.95, 73.76, 81.40 ]
+	examples.each_with_index do | example , example_number |
 
-		before do
-			@co=Checkout.new(
-					[
-						RuleLavender.new(),
-						RuleSixtyOrMore.new()
-				]
-			)
+		basket=''
+		for code in example
+			basket+="#{code},"
 		end
 
-		examples.each_with_index do | example , example_number |
-			basket=''
-			for code in example
-				basket+="#{code},"
-			end
-			basket=basket.chomp( "," )
+		describe "Basket #{ basket.chomp( ',' ) }" do
+			it 'Has the expected price' do
 
-			describe "Basket #{ basket }" do
-				before do
-					for code in example
-						@co.scanItem( items[code] )
-					end
+				for code in example
+					co.scanItem( items[code] )
 				end
-				it "Has the expected price" do
-					@co.total.must_equal results[ example_number ]
-				end
+
+				co.total.must_equal results[ example_number ]
 			end
 		end
 	end
