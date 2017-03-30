de9a174c7c159198646a0860cefd3b791575be52
Nadal Gonzalo García Zavala <snorkellingcactus@gmail.com>
2017-03-30 02:39:16 -0300

    Final Solution

--- Checkout.rb
+++ Checkout.rb
@@ -7,42 +7,29 @@ class Checkout
 
 	def initialize( rules )
 		@rules=rules
-
-		@items=[]
-		
-		@total=0
-	end
-
-	def calcTotal()
 		@total_with_rules=0
-
-		for rule in @rules
-			rule.reset
-		end
-
-		for item in @items
-			@last_scanned_item=item
-			@total_with_rules+= item.price
-
-			for rule in @rules
-				rule.canApply( self )
-			end
-		end
-
-		for rule in @rules
-			@total_with_rules+=rule.getFinalPriceDiff( self )
-		end
-
-		return @total_with_rules
 	end
 
 	def total
-		return Float ( sprintf( '%.2f', calcTotal ) )
+
+		for rule in @rules
+			@total_with_rules+=rule.getFinalPriceDiff( self )
+			rule.reset
+		end
+
+		result=Float ( sprintf( '%.2f', @total_with_rules ) )
+
+		@total_with_rules=0
+
+		return result
 	end
 
 	def scanItem( item )
-		@items.push( item )
-	end
+		@last_scanned_item=item
+		@total_with_rules+= item.price
 
-	private :calcTotal
+		for rule in @rules
+			rule.canApply( self )
+		end
+	end
 end
